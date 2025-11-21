28da9ac3b0914e9b4190f3019eae4813337756e1
Nirmal <nsr.kpma@gmail.com>
2025-11-21 18:07:22 +0530

    added allure support and modified extent report looks

---
+++ src/test/java/Kane/SeleniumJavaFramework/tests/SampleTest.java
@@ -0,0 +1,10 @@
+package Kane.SeleniumJavaFramework.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test (groups= {"POC"})
+    public void test1() {
+        System.out.println("Running sample test");
+    }
+}
